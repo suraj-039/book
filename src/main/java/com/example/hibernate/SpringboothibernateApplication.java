027727bd6964f1783a0f9cf3de45e17203d258b4
suraj-039 <surajmuthyam5@gmail.com>
2023-02-13 13:03:59 +0530

    changes 1

--- src/main/java/com/example/hibernate/SpringboothibernateApplication.java
+++ src/main/java/com/example/hibernate/SpringboothibernateApplication.java
@@ -12,7 +12,9 @@ import io.swagger.v3.oas.annotations.info.Info;
 @OpenAPIDefinition(info=@Info(description = "This is a book class",version = "1.0",
 					title = "Spring API"))
 public class SpringboothibernateApplication {
-
+//hai
+	//
+	///
 	public static void main(String[] args) {
 		SpringApplication.run(SpringboothibernateApplication.class, args);
 	}
